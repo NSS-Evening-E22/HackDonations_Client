46f0cf695926eb7f278a2bce7b15a414ff08ab12
paulpatterson <paul@pattersonmultimedia.com>
2023-10-26 20:45:55 -0500

    minor tweaks for shell2

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { Button } from 'react-bootstrap';
+import { getOrganizations } from '../api/organizationData';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import OrganizationCard from '../components/OrganizationCard';
 
 function Home() {
   const { user } = useAuth();
 
+  const [organizations, setOrganizations] = useState([]);
+
+  const getAllTheOrganizations = () => {
+    getOrganizations(user.uid).then(setOrganizations);
+  };
+
+  useEffect(() => {
+    getAllTheOrganizations();
+  }, []);
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
     >
+      <Link href="/organization/new" passHref>
+        <Button>Add A Organization</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {organizations.map((organization) => (
+          <OrganizationCard key={organization.firebaseKey} obj={organization} onUpdate={getAllTheOrganizations} />
+        ))}
+      </div>
       <h1>Hello {user.displayName}! </h1>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
